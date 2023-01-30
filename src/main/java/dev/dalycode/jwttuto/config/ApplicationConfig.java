9eae793e55870bdcb584ac5bcd86d7ec581b8af4
DalyChouikh <chouikhdaly215@gmail.com>
2023-01-30 23:45:54 +0100

    Added Authentication Manager

--- src/main/java/dev/dalycode/jwttuto/config/ApplicationConfig.java
+++ src/main/java/dev/dalycode/jwttuto/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import dev.dalycode.jwttuto.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +32,12 @@ public class ApplicationConfig {
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
