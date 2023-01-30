7a117b1a7f422613f29deb4f368c40e18fe83c1d
DalyChouikh <chouikhdaly215@gmail.com>
2023-01-30 22:59:56 +0100

    Added ApplicationConfig

---
+++ src/main/java/dev/dalycode/jwttuto/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package dev.dalycode.jwttuto.config;
+
+import dev.dalycode.jwttuto.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + username + " not found"));
+    }
+
+}
