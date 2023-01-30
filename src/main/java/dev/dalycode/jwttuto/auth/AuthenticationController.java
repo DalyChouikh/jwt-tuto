8f691321a9ba5702a4bc0bf3123995d676ff4720
DalyChouikh <chouikhdaly215@gmail.com>
2023-01-31 00:28:31 +0100

    Added AuthenticationController

--- src/main/java/dev/dalycode/jwttuto/auth/AuthenticationController.java
+++ src/main/java/dev/dalycode/jwttuto/auth/AuthenticationController.java
@@ -1,2 +1,27 @@
-package dev.dalycode.jwttuto.auth;public class AuthenticationController {
+package dev.dalycode.jwttuto.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> response(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> response(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
 }
